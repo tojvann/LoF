c59108fca2966d17453f313923cce187f8c357ef
tojvann <bravesingh2@gmail.com>
2020-09-17 00:49:18 +0530

    Conditionals

---
+++ Getting Started/conditionals.java
@@ -0,0 +1,11 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int x = 11;
+        if (x % 2 == 0) {
+            System.out.println(x + " is Even");
+        } else {
+            System.out.println(x + " is Odd");
+        }
+        System.out.println("Hard Work is better than Smart Work!");
+    }
+}
---
+++ Getting Started/conditionals2.java
@@ -0,0 +1,13 @@
+public class conditionals2 {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+        if (n1 == n2) {
+            System.out.println(n1 + " is equal to " + n2);
+        } else if (n1 > n2) {
+            System.out.println(n1 + " is greater than " + n2);
+        } else {
+            System.out.println(n1 + " is less than " + n2);
+        }
+    }
+}
