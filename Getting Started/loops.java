bc8e9a239967e4f1943c98f6edabf30985bf5544
tojvann <bravesingh2@gmail.com>
2020-09-17 01:22:07 +0530

    Loops

---
+++ Getting Started/loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String[] args) {
+        /*
+         * int i = 1; while (i <= 9) { System.out.println(i); i++; }
+         * 
+         */
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Done.");
+    }
+
+}
