d724364f93239db0abfd7d2502ac822a4a89033d
tojvann <bravesingh2@gmail.com>
2020-09-17 00:11:01 +0530

    First Commit

---
+++ Getting Started/Hello.java
@@ -0,0 +1,9 @@
+//How to print is java
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("*\n**\n***\n****\n*****\n");
+    }
+    
+}
---
+++ Getting Started/printZ.java
@@ -0,0 +1,6 @@
+public class printZ {
+    public static void main(String[] args){
+        System.out.print("*****\n   *\n  *\n *\n*****\n");
+    }
+    
+}
